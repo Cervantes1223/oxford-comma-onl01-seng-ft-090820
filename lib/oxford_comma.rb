91f14fd46f99f005e82424963c8977a6273d51fc
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-13 23:35:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,2 +1,9 @@
 def oxford_comma(kiwi)
- 
+ if array.length == 1
+end 	    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    return "#{array[0..-2].join(", ")}, and #{array.last}"
+  end
+end
